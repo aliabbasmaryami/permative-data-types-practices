a9fffaee69fa4df3a24c6f311bda0eed1905cb7f
Ali abbas <94835254+aliabbasmaryami@users.noreply.github.com>
2023-03-03 16:15:17 +0500

    Add files via upload

---
+++ practice.java/js/script.js
@@ -0,0 +1,17 @@
+let a = null;
+let b = undefined;
+let c = 123;
+let d = BigInt("231");
+let e = "ali";
+let f = true;
+let g = Symbol("iam a symbol of js");
+console.log(a,b,c,d,e,f,g)
+console.log(c)
+
+const item = {
+    "name": "ali",
+    "id": 2097,
+    "address": "nalter"
+}
+console.log(item)
+console.log(item["address"])
